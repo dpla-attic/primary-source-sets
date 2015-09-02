619d421f46952c049fe82ff891bf159c84c7a240
Mark A. Matienzo <mark@dp.la>
2015-09-02 17:55:11 -0400

    Use mount location consistently for assets in development and production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,9 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  # Use the relative URL to build the paths for assets, too, please...
+  config.action_controller.relative_url_root = Settings.relative_url_root
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
