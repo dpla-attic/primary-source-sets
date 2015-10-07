fdd094493e2b29c4752961a0883dfb605c5457a1
Mark Breedlove <mb@dp.la>
2015-10-07 14:59:15 -0400

    Fix file_base regex in upload.js

--- app/assets/javascripts/upload.js
+++ app/assets/javascripts/upload.js
@@ -62,7 +62,7 @@
 
         // For file basename, get rid of "video/" or "audio/" path part and
         // file extension.
-        file_base = key.replace(/^[a-z]+\/(.*)\.[a-z]+$/i, "$1");
+        file_base = key.replace(/^[a-z]+\/(.*)\.[a-z0-9]+$/i, "$1");
 
         postdata = {};
         postdata[pss_asset_type] = {file_base: file_base, key: key}
