fe179cef0ec9706ac217c73e278e29af6532a6ee
Audrey Altman <audrey@dp.la>
2015-10-08 13:44:25 -0400

    add frontend_path method

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 describe ApplicationHelper, type: :helper do
    
-   describe '#markdown' do
+  describe '#markdown' do
 
-     it 'renders HTML from a markdown String' do
-       text = '**Moomin!**'
-       expect(helper.markdown(text)).to eq("<p><strong>Moomin!</strong></p>\n")
+    it 'renders HTML from a markdown String' do
+      text = '**Moomin!**'
+      expect(helper.markdown(text)).to eq("<p><strong>Moomin!</strong></p>\n")
+    end
+
+    it 'raises a TypeError if input is not a String' do
+      invalid_input = 123
+      expect{ helper.markdown(invalid_input) }.to raise_error(TypeError)
      end
+  end
 
-     it 'raises a TypeError if input is not a String' do
-       invalid_input = 123
-       expect{ helper.markdown(invalid_input) }.to raise_error(TypeError)
-     end
-   end
-
-   describe '#inline_markdown' do
+  describe '#inline_markdown' do
 
     it 'strips enclosing <p> tags' do
       text = '**Moomin!**'
@@ -26,6 +26,14 @@ describe ApplicationHelper, type: :helper do
       text = '<p>Moomin!</p><p>Snorkmaiden</p>'
       expect(helper.inline_markdown(text))
         .to eq("<p>Moomin!</p><p>Snorkmaiden</p>\n")
+    end
+  end
+
+  describe '#frontend_path' do
+    it 'constructs a URI with correct /s' do
+      allow(Settings).to receive_message_chain(:frontend, :url)
+        .and_return 'http://example.com/'
+      expect(helper.frontend_path('/path')).to eq 'http://example.com/path'
     end
   end
 end
