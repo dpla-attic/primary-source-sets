9782e66dff0483763b8a551f3a8c645e487e4717
Audrey Altman <audrey@dp.la>
2015-10-09 15:59:02 -0400

    remove a/v edit views

--- spec/controllers/audios_controller_spec.rb
+++ spec/controllers/audios_controller_spec.rb
@@ -6,7 +6,7 @@ describe AudiosController, type: :controller do
   let(:attributes) { attributes_for(:audio_factory) }
   let(:invalid_attributes) { attributes_for(:invalid_audio_factory) }
 
-  it_behaves_like 'admin-only route', :index, :show, :new, :edit, :create
+  it_behaves_like 'admin-only route', :index, :show, :new, :create
 
   context 'admin logged in' do
     login_admin
