76f9564bc843f501bd354f13674ab7f526db10cd
Mark Breedlove <mb@dp.la>
2015-10-16 12:18:07 -0400

    Add image viewer
    Add Lightbox2 image viewer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.precompile += %w( docupload.js )
 Rails.application.config.assets.precompile += %w( imgupload.js )
 Rails.application.config.assets.precompile += %w( form.js )
 Rails.application.config.assets.precompile += %w( style.js )
+Rails.application.config.assets.precompile += %w( lightbox.js )
 
 # Precompile assets from gems
 Rails.application.config.assets.precompile += %w( dpla-colors.css dpla-fonts.css )
