b669987c752d0a8a4e9ab77a2997c7350d3749e2
Audrey Altman <audrey@dp.la>
2015-10-16 09:32:12 -0400

    add destroy depenedency to assets-attachments reltionship

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,5 @@
 class Document < ActiveRecord::Base
-  has_many :attachments, as: :asset
+  has_many :attachments, as: :asset, dependent: :destroy
   has_many :sources, through: :attachments
   validates :file_name, presence: :true
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,5 @@
 class Video < ActiveRecord::Base
-  has_many :attachments, as: :asset
+  has_many :attachments, as: :asset, dependent: :destroy
   has_many :sources, through: :attachments
   validates :file_base, presence: :true
 end
