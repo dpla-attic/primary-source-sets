55d911d9801848997a05aa090ef9566347daddac
Audrey Altman <audrey@dp.la>
2016-02-29 17:04:55 -0500

    views for reviewers

--- spec/views/source_sets/index.html.erb_spec.rb
+++ spec/views/source_sets/index.html.erb_spec.rb
@@ -20,4 +20,55 @@ describe 'source_sets/index.html.erb', type: :view do
     render
     expect(rendered).not_to include('Moomin')
   end
+
+  context 'logged in manager' do
+    before do
+      admin = create(:admin_factory)
+      sign_in admin
+    end
+
+    it 'shows unpublished sets' do
+      render
+      expect(rendered).to include('Moomin')
+    end
+
+    it 'links to source_set#new' do
+      render
+      expect(rendered).to include("<a href=\"#{new_source_set_path}\"")
+    end
+  end
+
+  context 'logged in editor' do
+    before do
+      admin = create(:editor_admin_factory)
+      sign_in admin
+    end
+
+    it 'shows unpublished sets' do
+      render
+      expect(rendered).to include('Moomin')
+    end
+
+    it 'links to source_set#new' do
+      render
+      expect(rendered).to include("<a href=\"#{new_source_set_path}\"")
+    end
+  end
+
+  context 'logged in reviewer' do
+    before do
+      admin = create(:reviewer_admin_factory)
+      sign_in admin
+    end
+
+    it 'shows unpublished sets' do
+      render
+      expect(rendered).to include('Moomin')
+    end
+
+    it 'does not link to source_set#new' do
+      render
+      expect(rendered).not_to include("<a href=\"#{new_source_set_path}\"")
+    end
+  end
 end
