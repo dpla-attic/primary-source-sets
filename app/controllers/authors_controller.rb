7e8a68f99a1833bada5cc85fc96a4e85bd99a4b4
Audrey Altman <audrey@dp.la>
2016-03-30 13:07:06 -0400

    add author for specified set or guide

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -15,6 +15,8 @@ class AuthorsController < ApplicationController
 
   def new
     @author = Author.new
+    @author.source_set_ids = [params[:source_set_id]]
+    @author.guide_ids = [params[:guide_id]]
   end
 
   def edit
--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -14,5 +14,21 @@ describe AuthorsController, type: :controller do
     it_behaves_like 'basic controller', :index, :show, :create, :update,
                                         :destroy
     it_behaves_like 'redirecting controller', :create
+
+    describe '#new' do
+      it 'adds source_set_id from param' do
+        source_set = create(:source_set_factory)
+        get :new, source_set_id: source_set.id
+        expect(assigns(:author).source_set_ids)
+          .to contain_exactly(source_set.id)
+      end
+
+      it 'adds guide_id from param' do
+        guide = create(:guide_factory)
+        get :new, guide_id: guide.id
+        expect(assigns(:author).guide_ids)
+          .to contain_exactly(guide.id)
+      end
+    end
   end
 end
