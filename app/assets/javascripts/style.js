28bef2861b863775d480d8c431c04489fc5b1b7e
Audrey Altman <audrey@dp.la>
2016-04-25 13:39:25 -0400

    css for dimensional manipulations source, source and set show views

--- app/assets/javascripts/style.js
+++ app/assets/javascripts/style.js
@@ -12,38 +12,13 @@
    });
 
   $(document).ready(function() {
-    setAsideHeight();
     setNameContainerHeight();
   });
 
   $(window).resize(function() {
-    setAsideHeight();
     setNameContainerHeight();
   })
 })();
-
-/*
- * Extend the height of the aside module to be at least as tall as the rendered
- * media asset.
- */
-function setAsideHeight() {
-  var height = $('.source aside .module').outerHeight();
-  var minHeight = $('.source .media-inner-container').height();
-  if (minHeight > height) {
-    $('.source aside .module').outerHeight(minHeight);
-  }
-
-  // resize header on set page
-  var moduleHeight = $('.set .guide-link .module').outerHeight();
-  var titleHeight = $('.set .title-outer-container').outerHeight();
-
-  if (moduleHeight < titleHeight) {
-    $('.set .guide-link .module').outerHeight(titleHeight);
-  }
-  if (titleHeight < moduleHeight) {
-    $('.set .title-outer-container').outerHeight(moduleHeight);
-  }
-}
 
 /*
  * This dynamically styles the boxes (class = set-name-container) that contain
